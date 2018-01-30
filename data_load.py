b8bb403f02f4d312c4824a92373e932fce06ee9c
Kyubyong Park <longinglove@nate.com>
2018-01-31 00:58:27 +0900

    Add files via upload

--- data_load.py
+++ data_load.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #/usr/bin/python2
 '''
-By kyubyong park. kbpark.linguist@gmail.com. 
-https://www.github.com/kyubyong/dc_tts
+By kyubyong park. kbpark.linguist@gmail.com.
+https://www.github.com/kyubyong/vq-vae
 '''
 
 from __future__ import print_function
 
-from hyperparams import Hyperparams as hp
+from hparams import Hyperparams as hp
 import numpy as np
 import tensorflow as tf
 from utils import *
@@ -15,108 +15,51 @@ import codecs
 import re
 import os
 import unicodedata
+import glob
 
-def load_vocab():
-    char2idx = {char: idx for idx, char in enumerate(hp.vocab)}
-    idx2char = {idx: char for idx, char in enumerate(hp.vocab)}
-    return char2idx, idx2char
+def speaker2id(speaker):
+    func = {speaker:id for id, speaker in enumerate(hp.speakers)}
+    return func.get(speaker, None)
 
-def text_normalize(text):
-    text = ''.join(char for char in unicodedata.normalize('NFD', text)
-                           if unicodedata.category(char) != 'Mn') # Strip accents
-
-    text = text.lower()
-    text = re.sub("[^{}]".format(hp.vocab), " ", text)
-    text = re.sub("[ ]+", " ", text)
-    return text
+def id2speaker(id):
+    func = {id:speaker for id, speaker in enumerate(hp.speakers)}
+    return func.get(id, None)
 
 def load_data(mode="train"):
     '''Loads data
       Args:
           mode: "train" or "synthesize".
     '''
-    # Load vocabulary
-    char2idx, idx2char = load_vocab()
-
     if mode=="train":
-        if "LJ" in hp.data:
-            # Parse
-            fpaths, text_lengths, texts = [], [], []
-            transcript = os.path.join(hp.data, 'transcript.csv')
-            lines = codecs.open(transcript, 'r', 'utf-8').readlines()
-            for line in lines:
-                fname, _, text = line.strip().split("|")
-
-                fpath = os.path.join(hp.data, "wavs", fname + ".wav")
-                fpaths.append(fpath)
-
-                text = text_normalize(text) + "E"  # E: EOS
-                text = [char2idx[char] for char in text]
-                text_lengths.append(len(text))
-                texts.append(np.array(text, np.int32).tostring())
-
-            return fpaths, text_lengths, texts
-        else: # nick or kate
-            # Parse
-            fpaths, text_lengths, texts = [], [], []
-            transcript = os.path.join(hp.data, 'transcript.csv')
-            lines = codecs.open(transcript, 'r', 'utf-8').readlines()
-            for line in lines:
-                fname, _, text, is_inside_quotes, duration = line.strip().split("|")
-                duration = float(duration)
-                if duration > 10. : continue
-
-                fpath = os.path.join(hp.data, fname)
-                fpaths.append(fpath)
-
-                text += "E"  # E: EOS
-                text = [char2idx[char] for char in text]
-                text_lengths.append(len(text))
-                texts.append(np.array(text, np.int32).tostring())
-
-        return fpaths, text_lengths, texts
-
-    else: # synthesize on unseen test text.
-        # Parse
-        lines = codecs.open(hp.test_data, 'r', 'utf-8').readlines()[1:]
-        sents = [text_normalize(line.split(" ", 1)[-1]).strip() + "E" for line in lines] # text normalization, E: EOS
-        texts = np.zeros((len(sents), hp.max_N), np.int32)
-        for i, sent in enumerate(sents):
-            texts[i, :len(sent)] = [char2idx[char] for char in sent]
-        return texts
+        files = glob.glob(hp.data)
+        speaker_ids = [speaker2id(os.path.basename(f)[:4]) for f in files]
+    else: # evaluation. samples.
+        files = ("/data/public/rw/datasets/VCTK-Corpus/wav48/p253/p253_410.wav")
+        speaker_ids = [speaker2id("p253")]
+    return files, speaker_ids
 
 def get_batch():
     """Loads training data and put them in queues"""
     with tf.device('/cpu:0'):
         # Load data
-        fpaths, text_lengths, texts = load_data() # list
-        maxlen, minlen = max(text_lengths), min(text_lengths)
+        files, speakers = load_data() # list
 
         # Calc total batch count
-        num_batch = len(fpaths) // hp.B
+        num_batch = len(files) // hp.batch_size
 
         # Create Queues
-        fpath, text_length, text = tf.train.slice_input_producer([fpaths, text_lengths, texts], shuffle=True)
+        f, speaker = tf.train.slice_input_producer([files, speakers], shuffle=True)
 
         # Parse
-        text = tf.decode_raw(text, tf.int32)  # (None,)
-        fname, mel, mag = tf.py_func(load_spectrograms, [fpath], [tf.string, tf.float32, tf.float32])  # (None, n_mels)
+        wav, qt = tf.py_func(get_wav, [f], [tf.float32, tf.int32])  # (T, 1)
 
         # Add shape information
-        fname.set_shape(())
-        text.set_shape((None,))
-        mel.set_shape((None, hp.n_mels))
-        mag.set_shape((None, hp.n_fft//2+1))
+        wav.set_shape((hp.length, 1))
+        qt.set_shape((hp.length, 1))
 
         # Batching
-        _, (texts, mels, mags, fnames) = tf.contrib.training.bucket_by_sequence_length(
-                                            input_length=text_length,
-                                            tensors=[text, mel, mag, fname],
-                                            batch_size=hp.B,
-                                            bucket_boundaries=[i for i in range(minlen + 1, maxlen - 1, 20)],
-                                            num_threads=8,
-                                            capacity=hp.B*4,
-                                            dynamic_pad=True)
+        speakers, wavs, qts = tf.train.batch(tensors=[speaker, wav, qt],
+                                                        batch_size=hp.batch_size,
+                                                        num_threads=32)
 
-    return texts, mels, mags, fnames, num_batch
-
+    return speakers, wavs, qts, num_batch
--- utils.py
+++ utils.py
@@ -1,162 +1,52 @@
-# -*- coding: utf-8 -*-
-#/usr/bin/python2
-'''
-By kyubyong park. kbpark.linguist@gmail.com. 
-https://www.github.com/kyubyong/dc_tts
-'''
-from __future__ import print_function, division
 
-import numpy as np
+from hparams import Hyperparams as hp
 import librosa
-import os, copy
-import matplotlib
-matplotlib.use('pdf')
-import matplotlib.pyplot as plt
-from scipy import signal
+import numpy as np
 
-from hyperparams import Hyperparams as hp
-import tensorflow as tf
-
-def get_spectrograms(fpath):
-    '''Parse the wave file in `fpath` and
-    Returns normalized melspectrogram and linear spectrogram.
+def mu_law_encode(audio):
+    '''Quantizes waveform amplitudes.
+    Mostly adaped from
+    https://github.com/ibab/tensorflow-wavenet/blob/master/wavenet/ops.py#L64-L75
 
     Args:
-      fpath: A string. The full path of a sound file.
-
-    Returns:
-      mel: A 2d array of shape (T, n_mels) and dtype of float32.
-      mag: A 2d array of shape (T, 1+n_fft/2) and dtype of float32.
+      audio: Raw wave signal. float32.
     '''
-    # Loading sound file
-    y, sr = librosa.load(fpath, sr=hp.sr)
+    mu = float(hp.quantization_channels - 1)
+    # Perform mu-law companding transformation (ITU-T, 1988).
+    # Minimum operation is here to deal with rare large amplitudes caused
+    # by resampling.
+    safe_audio_abs = min(np.abs(audio), 1.0)
+    magnitude = np.log1p(mu * safe_audio_abs) / np.log1p(mu)
+    signal = np.sign(audio) * magnitude
+    # Quantize signal to the specified number of levels.
+    return ((signal + 1) / 2 * mu + 0.5).astype(np.int32)
 
-    # Trimming
-    y, _ = librosa.effects.trim(y)
 
-    # Preemphasis
-    y = np.append(y[0], y[1:] - hp.preemphasis * y[:-1])
-
-    # stft
-    linear = librosa.stft(y=y,
-                          n_fft=hp.n_fft,
-                          hop_length=hp.hop_length,
-                          win_length=hp.win_length)
-
-    # magnitude spectrogram
-    mag = np.abs(linear)  # (1+n_fft//2, T)
-
-    # mel spectrogram
-    mel_basis = librosa.filters.mel(hp.sr, hp.n_fft, hp.n_mels)  # (n_mels, 1+n_fft//2)
-    mel = np.dot(mel_basis, mag)  # (n_mels, t)
-
-    # to decibel
-    mel = 20 * np.log10(np.maximum(1e-5, mel))
-    mag = 20 * np.log10(np.maximum(1e-5, mag))
-
-    # normalize
-    mel = np.clip((mel - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
-    mag = np.clip((mag - hp.ref_db + hp.max_db) / hp.max_db, 1e-8, 1)
-
-    # Transpose
-    mel = mel.T.astype(np.float32)  # (T, n_mels)
-    mag = mag.T.astype(np.float32)  # (T, 1+n_fft//2)
-
-    return mel, mag
-
-def spectrogram2wav(mag):
-    '''# Generate wave file from linear magnitude spectrogram
-
-    Args:
-      mag: A numpy array of (T, 1+n_fft//2)
-
-    Returns:
-      wav: A 1-D numpy array.
+def mu_law_decode(output):
+    '''Recovers waveform from quantized values.
+    Mostly adapted from
+    https://github.com/ibab/tensorflow-wavenet/blob/master/wavenet/ops.py#L64-L75
     '''
-    # transpose
-    mag = mag.T
+    mu = hp.quantization_channels - 1
+    # Map values back to [-1, 1].
+    signal = 2 * (output.astype(np.float32) / mu) - 1
+    # Perform inverse of mu-law transformation.
+    magnitude = (1 / mu) * ((1 + mu)**abs(signal) - 1)
+    return np.sign(signal) * magnitude
 
-    # de-noramlize
-    mag = (np.clip(mag, 0, 1) * hp.max_db) - hp.max_db + hp.ref_db
-
-    # to amplitude
-    mag = np.power(10.0, mag * 0.05)
-
-    # wav reconstruction
-    wav = griffin_lim(mag**hp.power)
-
-    # de-preemphasis
-    wav = signal.lfilter([1], [1, -hp.preemphasis], wav)
-
-    # trim
+def get_wav(fpath):
+    wav, sr = librosa.load(fpath, sr=hp.sr)
     wav, _ = librosa.effects.trim(wav)
+    wav /= np.abs(wav).max()
+    qt = mu_law_encode(wav)
 
-    return wav.astype(np.float32)
+    # Padding
+    maxlen = hp.duration * hp.sr
+    wav = np.pad(wav, ([0, maxlen]), mode="constant")[:maxlen]
+    qt = np.pad(qt, ([0, maxlen]), mode="constant")[:maxlen]
 
-def griffin_lim(spectrogram):
-    '''Applies Griffin-Lim's raw.'''
-    X_best = copy.deepcopy(spectrogram)
-    for i in range(hp.n_iter):
-        X_t = invert_spectrogram(X_best)
-        est = librosa.stft(X_t, hp.n_fft, hp.hop_length, win_length=hp.win_length)
-        phase = est / np.maximum(1e-8, np.abs(est))
-        X_best = spectrogram * phase
-    X_t = invert_spectrogram(X_best)
-    y = np.real(X_t)
+    ####
+    wav = np.expand_dims(wav, -1) # (T, 1)
+    qt = np.expand_dims(qt, -1) # (T, 1)
 
-    return y
-
-def invert_spectrogram(spectrogram):
-    '''Applies inverse fft.
-    Args:
-      spectrogram: [1+n_fft//2, t]
-    '''
-    return librosa.istft(spectrogram, hp.hop_length, win_length=hp.win_length, window="hann")
-
-def plot_alignment(alignment, gs, dir=hp.logdir):
-    """Plots the alignment.
-
-    Args:
-      alignment: A numpy array with shape of (encoder_steps, decoder_steps)
-      gs: (int) global step.
-      dir: Output path.
-    """
-    if not os.path.exists(dir): os.mkdir(dir)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(alignment)
-
-    fig.colorbar(im)
-    plt.title('{} Steps'.format(gs))
-    plt.savefig('{}/alignment_{}.png'.format(dir, gs), format='png')
-
-def guided_attention(g=0.2):
-    '''Guided attention. Refer to page 3 on the paper.'''
-    W = np.zeros((hp.max_N, hp.max_T), dtype=np.float32)
-    for n_pos in range(W.shape[0]):
-        for t_pos in range(W.shape[1]):
-            W[n_pos, t_pos] = 1 - np.exp(-(t_pos / float(hp.max_T) - n_pos / float(hp.max_N)) ** 2 / (2 * g * g))
-    return W
-
-def learning_rate_decay(init_lr, global_step, warmup_steps = 4000.0):
-    '''Noam scheme from tensor2tensor'''
-    step = tf.to_float(global_step + 1)
-    return init_lr * warmup_steps**0.5 * tf.minimum(step * warmup_steps**-1.5, step**-0.5)
-
-def load_spectrograms(fpath):
-    '''Read the wave file in `fpath`
-    and extracts spectrograms'''
-
-    fname = os.path.basename(fpath)
-    mel, mag = get_spectrograms(fpath)
-    t = mel.shape[0]
-
-    # Marginal padding for reduction shape sync.
-    num_paddings = hp.r - (t % hp.r) if t % hp.r != 0 else 0
-    mel = np.pad(mel, [[0, num_paddings], [0, 0]], mode="constant")
-    mag = np.pad(mag, [[0, num_paddings], [0, 0]], mode="constant")
-
-    # Reduction
-    mel = mel[::hp.r, :]
-    return fname, mel, mag
-
+    return wav[:-1, :], qt[1:, :] # why?
