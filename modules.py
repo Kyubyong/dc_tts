4f73d30818493f93da28f22a5da142d4fd95d519
Kyubyong Park <longinglove@nate.com>
2018-01-30 19:24:00 +0900

    Remove highwaynet which is obsolete

--- modules.py
+++ modules.py
@@ -64,30 +64,6 @@ def normalize(inputs,
     return outputs
 
 
-def highwaynet(inputs, num_units=None, scope="highwaynet", reuse=None):
-    '''Highway networks, see https://arxiv.org/abs/1505.00387
-
-    Args:
-      inputs: A 3D tensor of shape [N, T, W].
-      num_units: An int or `None`. Specifies the number of units in the highway layer
-             or uses the input size if `None`.
-      scope: Optional scope for `variable_scope`.
-      reuse: Boolean, whether to reuse the weights of a previous layer
-        by the same name.
-
-    Returns:
-      A 3D tensor of shape [N, T, W].
-    '''
-    if not num_units:
-        num_units = inputs.get_shape()[-1]
-
-    with tf.variable_scope(scope, reuse=reuse):
-        H = tf.layers.dense(inputs, units=num_units, activation=tf.nn.relu, name="dense1")
-        T = tf.layers.dense(inputs, units=num_units, activation=tf.nn.sigmoid,
-                            bias_initializer=tf.constant_initializer(-1.0), name="dense2")
-        outputs = H * T + inputs * (1. - T)
-    return outputs
-
 def conv1d(inputs,
            filters=None,
            size=1,
